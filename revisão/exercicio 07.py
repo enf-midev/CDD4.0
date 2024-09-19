1720a128953ce533a468f49ac4892cb6a31f3ed6
enf-midev <enf.milenems@gmail.com>
2024-09-19 11:58:39 -0300

    atividades de revisão para prova

--- revisão/exercicio 07.py
+++ revisão/exercicio 07.py
@@ -1,2 +1,6 @@
 for x in range (1 ,500+1, 1):
+    if x % 2 != 0:
+        print(x, end=" ")
+# existe outro modo de fazer também:
+for x in range (1, 500+1, 2):
     print(x, end=" ")
---
+++ revisão/exercicio 10.py
@@ -0,0 +1,5 @@
+rep = 100
+while rep >=1 :
+    if rep % 2 !=0:
+        print (rep)
+    rep = rep - 1
