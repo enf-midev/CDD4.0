585075ff80cdd6c952982c1433097055e3a90227
enf-midev <enf.milenems@gmail.com>
2024-09-19 09:07:00 -0300

    teste

---
+++ revisão/exercicio 01.py
@@ -0,0 +1,8 @@
+a = int(input("Informe o 1º número: "))
+b = int(input("Informe o 2º número: "))
+c = int(input("Informe o 3º número: "))
+soma = a + b
+if c > soma:
+    print(f" A soma é: {soma}. E menor do que {c}")
+else:
+    print(f"A soma é {soma}")
